78a36415412f9e8e3a4bf027530452627358fd0c
Nina Potrebich <potrebich@gmail.com>
2020-02-23 15:28:42 -0800

    Init project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,79 @@
-const userClick = () => {
-  console.log('click');
+// lyric info
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+// initial redux store
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
 }
+
+// reducer
+const reducer = (state = initialState, action) => {
+  let newState;
+  switch(action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState;
+    case 'RESTART_SONG':
+      newState = initialState;
+      return newState;
+    default:
+      return state;
+  }
+  return state;
+}
+
+// jest tests + setup will go here
+const { expect } = window;
+
+expect(reducer(initialState, {type: null})).toEqual(initialState);
+
+expect(reducer(initialState, {type: 'NEXT_LYRIC'})).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+})
+
+expect(reducer({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1,
+},
+{type: 'RESTART_SONG'})
+).toEqual(initialState);
+
+// redux store
+const { createStore } = Redux;
+const store = createStore(reducer);
+console.log(store.getState());
+
+// rendering state in DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = function() {
+  renderLyrics();
+}
+
+// click listener
+const userClick = () => {
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+    store.dispatch({type: 'RESTART_SONG'});
+  } else {
+    store.dispatch({type: 'NEXT_LYRIC'});
+  }
+
+}
+
+//subscribe to redux store
+store.subscribe(renderLyrics);
